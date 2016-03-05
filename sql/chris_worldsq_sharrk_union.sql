7fc2725e91f40f48ea3454d070bc4a24c0966653
Rebel Scum <linht@vulcan.com>
2016-03-04 17:28:39 -0800

    removed old code

--- sql/chris_worldsq_sharrk_union.sql
+++ sql/chris_worldsq_sharrk_union.sql
@@ -2,7 +2,6 @@ Select
 'Insert into sw_by_shark_grid
 Select s.shark_gid, st_union(sw_intersection)::geometry(MultiPolygon,4326) 
 from shark_worldsq_int s
-where s.shark_gid
-and s.gid between ' || g.id+1 || ' and ' || g.id+5 ||
+where s.shark_gid between ' || g.id+1 || ' and ' || g.id+5 ||
 ' group by s.shark_gid'
 from generate_series(0,(select max(shark_gid) from shark_worldsq_int), 5) g(id);
