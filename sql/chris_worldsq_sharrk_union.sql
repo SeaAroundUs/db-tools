0f45af5095bbf6b7ce933035fb83a94899bef124
cjhoorn <c.hoornaert@fisheries.ubc.ca>
2016-03-04 17:37:28 -0800

    3rd try

--- sql/chris_worldsq_sharrk_union.sql
+++ sql/chris_worldsq_sharrk_union.sql
@@ -4,4 +4,12 @@ Select s.shark_gid, st_union(sw_intersection)::geometry(MultiPolygon,4326)
 from shark_worldsq_int s
 where s.shark_gid between ' || g.id+1 || ' and ' || g.id+5 ||
 ' group by s.shark_gid'
-from generate_series(0,(select max(shark_gid) from shark_worldsq_int), 5) g(id);
+from generate_series(0,(select max(shark_gid) from shark_worldsq_int), 5) g(id);\
+
+Select
+'Insert into sw_by_world_gid
+Select s.world_gid, st_union(sw_intersection)::geometry(MultiPolygon,4326) 
+from shark_worldsq_int s
+where s.world_gid between ' || g.id+1 || ' and ' || g.id+5 ||
+' group by s.world_gid'
+from generate_series(0,(select max(world_gid) from shark_worldsq_int), 5) g(id);
