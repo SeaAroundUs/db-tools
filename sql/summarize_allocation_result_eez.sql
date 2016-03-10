df230894a90a1da2fb310306ce5bc71f21c7afd4
Rebel Scum <linht@vulcan.com>
2016-03-10 12:55:21 -0800

    Updated to fix a critical bug whereby the query was only inserting into three columns instead of the four columns that table has.

--- sql/summarize_allocation_result_eez.sql
+++ sql/summarize_allocation_result_eez.sql
@@ -1,8 +1,8 @@
-select format('insert into allocation.allocation_result_eez
-               select r.universal_data_id, a.inherited_att_belongs_to_reconstruction_eez_id, sum(r.allocated_catch)
+select format('insert into allocation.allocation_result_eez(universal_data_id,eez_id,fao_area_id,total_catch)
+               select r.universal_data_id, a.inherited_att_belongs_to_reconstruction_eez_id, a.fao_area_id, sum(r.allocated_catch)
                  from allocation.allocation_simple_area a 
                  join allocation_partition.allocation_result_%s as r on (r.allocation_simple_area_id = a.allocation_simple_area_id)
                 where a.inherited_att_belongs_to_reconstruction_eez_id > 0 
-                group by r.universal_data_id, a.inherited_att_belongs_to_reconstruction_eez_id', m.partition_id)
+                group by r.universal_data_id, a.inherited_att_belongs_to_reconstruction_eez_id, a.fao_area_id', m.partition_id)
   from allocation.allocation_result_partition_map m
  order by m.partition_id;
