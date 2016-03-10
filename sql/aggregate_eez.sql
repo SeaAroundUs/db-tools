b7165b78b0d0c6b42b858a16dedea1360d96a4a8
Rebel Scum <linht@vulcan.com>
2016-03-10 12:54:36 -0800

    Minor optimization by not return the eez_id in the inner query as we already know what it is by the outer context.

--- sql/aggregate_eez.sql
+++ sql/aggregate_eez.sql
@@ -3,9 +3,8 @@ select 'INSERT INTO web.v_fact_data(
           catch_type_id,catch_status,reporting_status,sector_type_id,catch_sum,real_value,primary_production_required,
           catch_trophic_level,catch_max_length
         )
-        WITH catch(main_area_id,sub_area_id,data_layer_id,year,taxon_key,fishing_entity_id, catch_type_id,catch_status,reporting_status,sector_type_id,total_catch,unit_price) AS (
-          SELECT ar.eez_id, 
-                 ar.fao_area_id,
+        WITH catch(sub_area_id,data_layer_id,year,taxon_key,fishing_entity_id,catch_type_id,catch_status,reporting_status,sector_type_id,total_catch,unit_price) AS (
+          SELECT ar.fao_area_id,
                  ad.data_layer_id,
                  ad.year,
                  ad.taxon_key, 
@@ -40,9 +39,8 @@ select 'INSERT INTO web.v_fact_data(
                sum(c.total_catch * t.sl_max)
 		  FROM catch c
           JOIN web.cube_dim_taxon t ON (t.taxon_key = c.taxon_key)
-          JOIN web.area a ON (a.marine_layer_id = 1 AND a.main_area_id = c.main_area_id AND a.sub_area_id = c.sub_area_id)
+          JOIN web.area a ON (a.marine_layer_id = 1 AND a.main_area_id = ' || t.eez_id || ' AND a.sub_area_id = c.sub_area_id)
 		  JOIN web.time tm ON (tm.time_business_key = c.year)
          GROUP BY c.sub_area_id, c.data_layer_id, c.fishing_entity_id, tm.time_key, c.year, c.taxon_key, a.area_key, c.sector_type_id, c.catch_type_id, c.catch_status, c.reporting_status'
   from (select distinct ar.eez_id from allocation.allocation_result_eez ar) as t
   order by t.eez_id;
-
