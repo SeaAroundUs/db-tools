7845761a03c8eadb8acbbbd01c1733765f130cd4
Rebel Scum <linht@vulcan.com>
2016-03-10 18:05:41 -0800

    Modified to also vacuum the just refreshed materialized views.

--- sql/refresh_web_mv.sql
+++ sql/refresh_web_mv.sql
@@ -3,3 +3,8 @@ union all
 (select 'refresh materialized view geo.' || table_name from matview_v('geo') where table_name not like 'TOTALS%')
 union all
 (select 'refresh materialized view allocation.' || table_name from matview_v('allocation') where table_name not like 'TOTALS%');
+(select 'vacuum analyze web.' || table_name from matview_v('web') where table_name not like 'TOTALS%' order by case when table_name = 'v_taxon_catch' then 1 else 0 end)
+union all
+(select 'vacuum analyze geo.' || table_name from matview_v('geo') where table_name not like 'TOTALS%')
+union all
+(select 'vacuum analyze allocation.' || table_name from matview_v('allocation') where table_name not like 'TOTALS%');
