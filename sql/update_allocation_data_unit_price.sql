a42e253e635b28fa0d67186db09b296255f79fe8
Rebel Scum <linht@vulcan.com>
2016-03-12 15:08:01 -0800

    Modified to handle the case when the unit_price column is already loaded with the default value but we found a match in the price table.

--- sql/update_allocation_data_unit_price.sql
+++ sql/update_allocation_data_unit_price.sql
@@ -1,5 +1,5 @@
 select 'update allocation.allocation_data ad set unit_price = p.price 
           from allocation.price p 
-         where ad.unit_price is null and ad.year = p.year and ad.fishing_entity_id = p.fishing_entity_id and ad.taxon_key = p.taxon_key and ad.year = ' || t.time_business_key
+         where ad.unit_price is distinct from p.price and ad.year = p.year and ad.fishing_entity_id = p.fishing_entity_id and ad.taxon_key = p.taxon_key and ad.year = ' || t.time_business_key
   from time t
  order by t.time_business_key;
