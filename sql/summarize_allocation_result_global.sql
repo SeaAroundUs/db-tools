e2d0593ae8ecaec89ce9a290684a0faf6182e3d2
Rebel Scum <linht@vulcan.com>
2016-03-15 18:33:56 -0700

    Fixed cut and paste error where the eez summary table is use in the query for high seas.

--- sql/summarize_allocation_result_global.sql
+++ sql/summarize_allocation_result_global.sql
@@ -13,4 +13,4 @@ select 'insert into allocation.allocation_result_global
           FROM allocation.allocation_result_high_seas
          where universal_data_id between ' || (g.idx+1) || ' and ' || (g.idx+200000) ||
        ' group by universal_data_id'
-  from generate_series((select min(universal_data_id)-1 from allocation_result_eez), (select max(universal_data_id) from allocation_result_eez), 200000) as g(idx);
+  from generate_series((select min(universal_data_id)-1 from allocation_result_high_seas), (select max(universal_data_id) from allocation_result_high_seas), 200000) as g(idx);
