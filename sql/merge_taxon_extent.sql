f555a44395494ed37231f33ac6d0fda02abc3a42
Rebel Scum <linht@vulcan.com>
2016-06-01 10:43:31 -0700

    Updated to use the local taxon_extent directory as default.

--- sql/merge_taxon_extent.sql
+++ sql/merge_taxon_extent.sql
@@ -1,1 +1,3 @@
-select format('select distribution.load_taxon_extent(''%s'')', table_name) from schema_v('taxon_dis') where table_name not like 'TOTALS%';
+select format('update log.extent_merge m set geom = (select st_multi(st_union(e.geom)) from distribution.taxon_extent e where e.taxon_key = %s) where taxon_key = %1$s', m.taxon_key)  
+  from log.extent_merge m
+ order by m.taxon_key;
