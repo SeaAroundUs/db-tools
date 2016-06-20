ff2d5f7466ba504f48bfd8bdc316c924100546e2
arash-t <a.tavakolie@fisheries.ubc.ca>
2016-06-20 14:17:16 -0700

    adding TRUNCATE & VACUUM

--- sql/populate_catch_data_in_csv.sql
+++ sql/populate_catch_data_in_csv.sql
@@ -1,3 +1,5 @@
-select 'insert into web_cache.catch_csv_4_' || rfmo_id || '(entity_layer_id, entity_id, csv_data) select 4, ' || rfmo_id || ', f_catch_data_in_csv(array[' || rfmo_id || '],4)' 
-  from rfmo 
+select format('TRUNCATE TABLE web_cache.catch_csv_4_%s;', rfmo_id) ||
+  format('insert into web_cache.catch_csv_4_%s(entity_layer_id, entity_id, csv_data) select 4, %1$s, web.f_catch_data_in_csv(array[%1$s], 4)', rfmo_id) ||
+  format(';VACUUM ANALYZE  web_cache.catch_csv_4_%s;', rfmo_id)
+  from web.rfmo
  where rfmo_id != 9;
