beb6fb3d5e61a36751227a9f41202ed6e06251fa
Rebel Scum <linht@vulcan.com>
2016-09-24 18:45:34 -0700

    Updated to re-use ui code from tkinter_util. Also, move aggregateAllPartition button to its own panel for a more organized look-and-feel.

--- cell_catch.py
+++ cell_catch.py
@@ -13,15 +13,19 @@ class CellCatchCommandPane(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.dbPane = dbPane
         self.buttonsPerRow = 10
+        self.parent = parent
 
         # Placeholders, these will really be initialized lazily latter, once all db parameters are available.
         self.yearList = None
                                                   
-        self.cmdFrame = add_label_frame(parent, "Aggregate Cell Catch", 100, 250)
+        self.cmdFrame = add_label_frame(parent, "Aggregate Cell Catch", 100, 220)
 
         parent.add(tk.Button(parent, text="Get list of cell catch year partitions to aggregate", fg="red", command=self.setupCommandPane))
 
         parent.add(self.cmdFrame)
+
+        self.aggregateAllBt = None
+        self.fillerPane = None
 
     def setupCommandPane(self):
         if not self.dbPane.isConnectionTestedSuccessfully():
@@ -64,22 +68,38 @@ class CellCatchCommandPane(tk.Frame):
                 " WHERE table_name NOT LIKE 'TOTALS%'" +
                 " ORDER BY 1").fetchall()
 
-            color = "blue"
+            row = 0
+            button_data = []
+
             for par in self.yearList:
-                self.createCommandButton(self.cmdFrame, par.year, row, column, color)
+                button_data.append([par.year, partial(self.processYearPartition, par.year), "blue"])
                 column += 1
 
                 if column >= self.buttonsPerRow:
+                    add_buttons(self.cmdFrame, button_data, row, 0, "horizontal")
+                    button_data = []
                     column = 0
                     row += 1
+
+            if button_data != []:
+                add_buttons(self.cmdFrame, button_data, row, 0, "horizontal")
+                row += 1
+
+            # Calling maintain cell catch just in case the cell catch partitions are not present for any reason
+            dbSession.execute("SELECT web_partition.maintain_cell_catch_partition()")
         finally:
             dbSession.close()
 
-        tk.Button(self.cmdFrame, text="All", fg="red", command=self.aggregateAllPartition).grid(
-            column=column, row=row, sticky=E)
-
         for child in self.cmdFrame.winfo_children():
             child.grid_configure(padx=5, pady=5)
+
+        if self.aggregateAllBt == None:
+            self.aggregateAllBt = tk.Button(self.parent, text="Aggregate All Year Partitions", fg="red", command=self.aggregateAllPartition, height=1)
+            self.parent.add(self.aggregateAllBt)
+
+            # Adding a filler pane for look only
+            self.fillerPane = ttk.Panedwindow(self.parent, orient=VERTICAL)
+            self.parent.add(self.fillerPane)
 
     def createCommandButton(self, parent, year, gRow, gColumn, color):
         tk.Button(parent, name=str(year), text=str(year), fg=color, command=partial(self.processYearPartition, year)).grid(
