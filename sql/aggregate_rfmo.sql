8fb8fb648f136ecf2256bd12b1879cff4f2cd408
QQ-Sortiz <s.ortiz@q-quatics.org>
2018-11-16 10:27:40 -0800

    Arranged end_use field on aggregation
    Arranged end_use field on aggregation
    -aggregate_eez
    -aggregate_global
    -aggregate_high_seas
    -aggregate_lme
    -aggregate_meow
    -aggregate_rfmo

--- sql/aggregate_rfmo.sql
+++ sql/aggregate_rfmo.sql
@@ -1,7 +1,7 @@
 select 'INSERT INTO web.v_fact_data(
           main_area_id,sub_area_id,marine_layer_id, data_layer_id, fishing_entity_id,gear_id,time_key,year,taxon_key,area_key,
-          catch_type_id,catch_status,reporting_status_id,reporting_status,sector_type_id,catch_sum,real_value,primary_production_required,
-          catch_trophic_level,catch_max_length, end_use_type_id
+          catch_type_id,catch_status,reporting_status_id,reporting_status,sector_type_id,end_use_type_id,catch_sum,real_value,primary_production_required,
+          catch_trophic_level,catch_max_length
         )
       WITH catch(main_area_id,year,taxon_key, data_layer_id, fishing_entity_id, gear_id, catch_type_id,reporting_status_id,sector_type_id,total_catch,unit_price, end_use_type_id) AS (
         SELECT ar.rfmo_id,
@@ -13,9 +13,9 @@ select 'INSERT INTO web.v_fact_data(
                  ad.catch_type_id,
                  ad.reporting_status_id,
                  ad.sector_type_id,
+				 coalesce (e.end_use_type_id ,1) as end_use_type_id,
                  ar.total_catch *  (coalesce (e.end_use_percentage,null, 1)),
-                 ad.unit_price,
-				 coalesce (e.end_use_type_id ,1) as end_use_type_id
+                 ad.unit_price
 		    FROM allocation.allocation_result_rfmo ar
 		    JOIN allocation.allocation_data ad ON (ad.universal_data_id = ar.universal_data_id)
 			left join web.end_use e ON e.fishing_entity_id = ad.fishing_entity_id 
@@ -42,12 +42,12 @@ select 'INSERT INTO web.v_fact_data(
                c.reporting_status_id,
                MAX(rs.abbreviation),
                c.sector_type_id,
+			   c.end_use_type_id,
                sum(c.total_catch), 
                sum(c.unit_price * c.total_catch),
                web.ppr(sum(c.total_catch), MAX(t.tl)),
                sum(c.total_catch * t.tl),
-               sum(c.total_catch * t.sl_max),
-			   c.end_use_type_id
+               sum(c.total_catch * t.sl_max)
 		  FROM catch c
           JOIN web.cube_dim_taxon t ON (t.taxon_key = c.taxon_key)
           JOIN web.area a ON (a.marine_layer_id = 4 AND a.main_area_id = c.main_area_id)
