b06b55d45895f15d73e7871f2f6163b6d8f734bf
Miel <s.ortiz@q-quatics.org>
2020-10-12 14:20:30 +0800

    Added condition for manual override
    Added condition for manual override in taxon distribution substitute

--- sql/insert_recon_catch_from_raw_catch.sql
+++ sql/insert_recon_catch_from_raw_catch.sql
@@ -63,7 +63,7 @@ rc.notes,
 rc.id
 from recon.raw_catch rc
 left join distribution.taxon_distribution_substitute tds on (tds.original_taxon_key = rc.taxon_key)
-where rc.id between %s and %s
+where rc.id between %s and %s and tds.is_manual_override = true
 ',
     idx+1,
     idx+50000
